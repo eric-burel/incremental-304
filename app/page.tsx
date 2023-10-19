fafe4a2c12ec12ab236935e2b26cdb7d5598f823
eric-burel <eb@lbke.fr>
2023-10-19 10:39:52 +0200

    show timestamp and add 10sec revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+export const revalidate = 10
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const intl = new Intl.DateTimeFormat("en-US", {dateStyle: "full", timeStyle:"medium"})
+  const now = intl.format(Date.now())
+  console.log("rendering home:", now)
+  return <div>{now}</div>
 }
